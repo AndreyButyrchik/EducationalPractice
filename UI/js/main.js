1509e0672d78140844958727ab75c992d1e98fd7
AndreyButyrchik <butyrchykandrey@gmail.com>
2018-03-03 21:20:22 +0300

    add func Get Photo Post by ID

---
+++ UI/js/main.js
@@ -0,0 +1,158 @@
+var photoPostsArray = [
+    {
+        id: '1',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Вася Пупкин',
+        photoLink: 'https://cdn.tinybuddha.com/wp-content/uploads/2016/05/Smiley-face-mug.jpg'
+    },
+    {
+        id: '2',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Dy8vgtarAFVdNRV1FkxHz3eMgp7W-QLVWZKPUoRTvrHkn_7H'
+    },
+    {
+        id: '3',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://www.success.com/sites/default/files/styles/article_main/public/main/articles/topofmind_positiveselftalk.jpg?itok=NFlk21hQ'
+    },
+    {
+        id: '4',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcM7AN7bS0HjKwrNy7mHAGcMWhgB1eR3EcIVrIl1iO9YypoBrBsQ'
+    },
+    {
+        id: '5',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://globalperspect.com/wp-content/uploads/2015/03/think-positive.jpg'
+    },
+    {
+        id: '6',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL18BBTTgIcNFFs35doGGhdr5MIa9nEbx15Fld3Zqb2aCXN7I6tA'
+    },
+    {
+        id: '7',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'https://cdn.pixabay.com/photo/2015/07/29/10/41/sun-flower-865734_960_720.jpg'
+    },
+    {
+        id: '8',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'https://i2.wp.com/father-hood.co.uk/wp-content/uploads/2017/10/smile.jpg?resize=845%2C523&ssl=1'
+    },
+    {
+        id: '9',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '10',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '11',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '12',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '13',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '14',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '15',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '16',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '17',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '18',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '19',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '20',
+        descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    }
+];
+(function() {
+    function getPhotoPost(id) {
+        if (id === undefined ||
+            typeof id !== "string" ||
+            Number(id) < 1 ||
+            Number(id) > photoPostsArray.length) {
+            return NaN;
+        }
+        for(var i = 0; i < photoPostsArray.length; i++){
+            if(photoPostsArray[i].id === id){
+                return photoPostsArray[i];
+            }
+        }
+        return NaN;
+    }
+})();
