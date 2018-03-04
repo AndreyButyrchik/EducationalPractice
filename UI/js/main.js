12845b86fc739cdad116789815b5be19c4d96175
AndreyButyrchik <butyrchykandrey@gmail.com>
2018-03-04 20:03:23 +0300

    add validate hashtags and likes

--- UI/js/main.js
+++ UI/js/main.js
@@ -2,142 +2,182 @@ var photoPostsArray = [
     {
         id: '1',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Вася Пупкин',
-        photoLink: 'https://cdn.tinybuddha.com/wp-content/uploads/2016/05/Smiley-face-mug.jpg'
+        createdAt: new Date('2018-03-02T23:00:00'),
+        author: 'Вася Сиплый',
+        photoLink: 'http://www.kino-teatr.ru/acter/album/3215/450605.jpg',
+        hashtags: ['#cool', '#smile', '#positive', '#intergalactic'],
+        likes: ['Дима Зевс', 'Зоя Кожаный-Затылок', 'Луи Кирпич', 'Шурик Веселый']
     },
     {
         id: '2',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-24T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Dy8vgtarAFVdNRV1FkxHz3eMgp7W-QLVWZKPUoRTvrHkn_7H'
+        createdAt: new Date('2018-03-01T23:00:00'),
+        author: 'Дима Зевс',
+        photoLink: 'https://pp.userapi.com/c305713/u11158447/154446725/x_60f14c4f.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '3',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-05-20T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://www.success.com/sites/default/files/styles/article_main/public/main/articles/topofmind_positiveselftalk.jpg?itok=NFlk21hQ'
+        createdAt: new Date('2018-01-12T23:00:00'),
+        author: 'Вано Питон',
+        photoLink: 'https://forum.anabolicshops.com/attachments/60185/',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '4',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-26T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcM7AN7bS0HjKwrNy7mHAGcMWhgB1eR3EcIVrIl1iO9YypoBrBsQ'
+        createdAt: new Date('2018-01-09T23:00:00'),
+        author: 'Ромчик Гвоздь',
+        photoLink: 'https://i08.fotocdn.net/s11/154/public_pin_m/474/2311969177.jpg',
+        hashtags: ['#cool', '#smile', '#positive', '#intergalactic'],
+        likes: ['Дима Зевс', 'Зоя Кожаный-Затылок', 'Луи Кирпич', 'Шурик Веселый']
     },
     {
         id: '5',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-27T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://globalperspect.com/wp-content/uploads/2015/03/think-positive.jpg'
+        createdAt: new Date('2018-03-03T23:00:00'),
+        author: 'Катя Пляжная',
+        photoLink: 'http://1.bp.blogspot.com/--BZ64SXerLA/U7Q4sosowBI/AAAAAAAADqs/K7poh-Rblq0/s1600/103350358_3256587_tynika0.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '6',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-05-20T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL18BBTTgIcNFFs35doGGhdr5MIa9nEbx15Fld3Zqb2aCXN7I6tA'
+        createdAt: new Date('2018-02-20T23:00:00'),
+        author: 'Фил Ночной',
+        photoLink: 'http://www.gipsyteam.ru/upload/Contentimage/default/1/4/9/14993.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '7',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-02T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'https://cdn.pixabay.com/photo/2015/07/29/10/41/sun-flower-865734_960_720.jpg'
+        createdAt: new Date('2018-02-18T23:00:00'),
+        author: 'Зоя Кожаный-Затылок',
+        photoLink: 'http://www.medpillmart.com/blog/wp-content/uploads/2016/03/healthy-life-with-exercise.jpg',
+        hashtags: ['#cool', '#smile', '#positive', '#intergalactic'],
+        likes: ['Дима Зевс', 'Зоя Кожаный-Затылок', 'Луи Кирпич', 'Шурик Веселый']
     },
     {
         id: '8',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-03T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'https://i2.wp.com/father-hood.co.uk/wp-content/uploads/2017/10/smile.jpg?resize=845%2C523&ssl=1'
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Димон Кудрявый',
+        photoLink: 'https://pp.userapi.com/c840226/v840226013/27508/MJjzdUOsaNQ.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '9',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-03-07T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-03-02T23:00:00'),
+        author: 'Луи Кирпич',
+        photoLink: 'https://s79369.cdn.ngenix.net/media/photo/original/20170515/5bf6a2b3b7b906ebd3e023192883b0f3.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '10',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-08T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-02-01T23:00:00'),
+        author: 'Павлик Подгубный',
+        photoLink: 'https://pp.userapi.com/c638326/v638326670/40cb7/Io79qfHCZnU.jpg',
+        hashtags: ['#cool', '#smile', '#positive', '#intergalactic'],
+        likes: ['Дима Зевс', 'Зоя Кожаный-Затылок', 'Луи Кирпич', 'Шурик Веселый']
     },
     {
         id: '11',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-05-20T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-02-02T23:00:00'),
+        author: 'Танюха Мазь',
+        photoLink: 'https://scontent-arn2-1.cdninstagram.com/t51.2885-15/e15/1170465_571301496276287_1328633538_n.jpg?ig_cache_key=NTg0NjA5MTk5MDA1NTgzNjUx.2',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '12',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-13T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-01-13T23:00:00'),
+        author: 'Ислам Чех',
+        photoLink: 'http://today.kz/static/uploads/5311aa79-96b1-4c5a-960f-37a1935c398c.jpeg',
+        hashtags: ['#cool', '#smile', '#positive', '#intergalactic'],
+        likes: ['Дима Зевс', 'Зоя Кожаный-Затылок', 'Луи Кирпич', 'Шурик Веселый']
     },
     {
         id: '13',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-11T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-01-11T23:00:00'),
+        author: 'Ульянка Куст',
+        photoLink: 'https://img-fotki.yandex.ru/get/202385/414521066.5/0_180fed_dc694d7f_orig.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '14',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-05-20T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-02-12T23:00:00'),
+        author: 'Леха Краснопёрый',
+        photoLink: 'http://lesohot.ru/upload/blogs/f92f2b517a886d5de80a4451ef38b218.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Дима Зевс', 'Зоя Кожаный-Затылок', 'Луи Кирпич', 'Шурик Веселый']
     },
     {
         id: '15',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-01T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-01-22T23:00:00'),
+        author: 'Шурик Веселый',
+        photoLink: 'https://союзженскихсил.рф/upload/resize_cache/main/b6d/800_800_1/b6d97c5b46ccc547e50b7210dc37c645.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '16',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-02T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-01-29T23:00:00'),
+        author: 'Даня Фазан',
+        photoLink: 'http://cs7004.vk.me/v7004018/1da5a/lwbInpNmCAM.jpg',
+        hashtags: ['#cool', '#smile', '#positive', '#intergalactic'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '17',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-14T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-01-11T23:00:00'),
+        author: 'Гена Орешек',
+        photoLink: 'https://news.pn/photo/3786ecbdc5eaa3fc92e9569a4bc5a8ec.i1200x900x687.jpeg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '18',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-15T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-03-01T23:00:00'),
+        author: 'Галя Печка',
+        photoLink: 'https://s10.stc.all.kpcdn.net/share/i/4/1174690/wx1080.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Дима Зевс', 'Зоя Кожаный-Затылок', 'Луи Кирпич', 'Шурик Веселый']
     },
     {
         id: '19',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-18T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-01-18T23:00:00'),
+        author: 'Саша Соска',
+        photoLink: 'http://blog.nedbright.com/wp-content/uploads/2015/10/1380443087_953250986.jpg',
+        hashtags: ['#cool', '#smile', '#positive'],
+        likes: ['Рома Торпеда', 'Галя Печка', 'Гена Орешек']
     },
     {
         id: '20',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-05-20T23:00:00'),
-        author: 'Иванов',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+        createdAt: new Date('2018-02-27T23:00:00'),
+        author: 'Рома Торпеда',
+        photoLink: 'https://i.ytimg.com/vi/7ApWJhEQaco/maxresdefault.jpg',
+        hashtags: ['#cool', '#smile', '#positive', '#intergalactic'],
+        likes: ['Дима Зевс', 'Зоя Кожаный-Затылок', 'Луи Кирпич', 'Шурик Веселый']
     }
 ];
 (function () {
@@ -160,7 +200,9 @@ var photoPostsArray = [
             typeof photoPost.descriprion !== "string" ||
             typeof photoPost.createdAt !== "object" ||
             typeof photoPost.author !== "string" ||
-            typeof photoPost.photoLink !== "string") {
+            typeof photoPost.photoLink !== "string" ||
+            typeof photoPost.hashtags !== "object" ||
+            typeof  photoPost.likes !== "object") {
             return false;
         }
         if (Number(photoPost.id) < 1 ||
@@ -177,6 +219,12 @@ var photoPostsArray = [
             return false;
         }
         if (Object.prototype.toString.call(photoPost.createdAt) !== "[object Date]") {
+            return false;
+        }
+        if (Object.prototype.toString.call(photoPost.hashtags) !== "[object Array]"){
+            return false;
+        }
+        if (Object.prototype.toString.call(photoPost.likes) !== "[object Array]"){
             return false;
         }
         for (var i = 0; i < photoPostsArray.length; i++) {
@@ -271,4 +319,6 @@ var photoPostsArray = [
 
         return filtPhotoPosts.slice(skip, skip + top);
     }
+
+    alert(Object.prototype.toString.call([]));
 })();
