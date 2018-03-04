4124da965ab793f222722753eba15fab982d99a0
AndreyButyrchik <butyrchykandrey@gmail.com>
2018-03-04 17:51:42 +0300

    add func getPhotoPosts

--- UI/js/main.js
+++ UI/js/main.js
@@ -9,184 +9,186 @@ var photoPostsArray = [
     {
         id: '2',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-24T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Dy8vgtarAFVdNRV1FkxHz3eMgp7W-QLVWZKPUoRTvrHkn_7H'
     },
     {
         id: '3',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-05-20T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://www.success.com/sites/default/files/styles/article_main/public/main/articles/topofmind_positiveselftalk.jpg?itok=NFlk21hQ'
     },
     {
         id: '4',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-26T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcM7AN7bS0HjKwrNy7mHAGcMWhgB1eR3EcIVrIl1iO9YypoBrBsQ'
     },
     {
         id: '5',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-27T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://globalperspect.com/wp-content/uploads/2015/03/think-positive.jpg'
     },
     {
         id: '6',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-05-20T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL18BBTTgIcNFFs35doGGhdr5MIa9nEbx15Fld3Zqb2aCXN7I6tA'
     },
     {
         id: '7',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-02T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'https://cdn.pixabay.com/photo/2015/07/29/10/41/sun-flower-865734_960_720.jpg'
     },
     {
         id: '8',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-03T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'https://i2.wp.com/father-hood.co.uk/wp-content/uploads/2017/10/smile.jpg?resize=845%2C523&ssl=1'
     },
     {
         id: '9',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-03-07T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '10',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-08T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '11',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-05-20T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '12',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-13T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '13',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-11T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '14',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-05-20T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '15',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-01T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '16',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-02T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '17',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-14T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '18',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-15T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '19',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
+        createdAt: new Date('2018-02-18T23:00:00'),
         author: 'Иванов Иван',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     },
     {
         id: '20',
         descriprion: 'Smile, it is the key that fits the lock of everybody\'s heart.',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Иванов Иван',
+        createdAt: new Date('2018-05-20T23:00:00'),
+        author: 'Иванов',
         photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
     }
 ];
-(function() {
+(function () {
     function getPhotoPost(id) {
         if (typeof id !== "string" ||
             Number(id) < 1 ||
             Number(id) > photoPostsArray.length) {
             return NaN;
         }
-        for(var i = 0; i < photoPostsArray.length; i++){
-            if(photoPostsArray[i].id === id){
+        for (var i = 0; i < photoPostsArray.length; i++) {
+            if (photoPostsArray[i].id === id) {
                 return photoPostsArray[i];
             }
         }
         return NaN;
     }
+
     function validatePhotoPost(photoPost) {
-        if( typeof photoPost.id !== "string" ||
-        typeof photoPost.descriprion !== "string" ||
-        typeof photoPost.createdAt !== "object" ||
-        typeof photoPost.author !== "string" ||
-        typeof photoPost.photoLink !== "string"){
+        if (typeof photoPost.id !== "string" ||
+            typeof photoPost.descriprion !== "string" ||
+            typeof photoPost.createdAt !== "object" ||
+            typeof photoPost.author !== "string" ||
+            typeof photoPost.photoLink !== "string") {
             return false;
         }
-        if(Number(photoPost.id) < 1 ||
-            parseInt(photoPost.id) !== Number(photoPost.id)){
+        if (Number(photoPost.id) < 1 ||
+            parseInt(photoPost.id) !== Number(photoPost.id)) {
             return false;
         }
-        if(photoPost.descriprion.length >= 200){
+        if (photoPost.descriprion.length >= 200) {
             return false;
         }
-        if(photoPost.author === ""){
+        if (photoPost.author === "") {
             return false;
         }
-        if(photoPost.photoLink === ""){
+        if (photoPost.photoLink === "") {
             return false;
         }
-        if(Object.prototype.toString.call(photoPost.createdAt) !== "[object Date]"){
+        if (Object.prototype.toString.call(photoPost.createdAt) !== "[object Date]") {
             return false;
         }
-        for(var i = 0; i < photoPostsArray.length; i++){
-            if((photoPostsArray[i].id === photoPost.id) && (photoPost !== photoPostsArray[i])){
+        for (var i = 0; i < photoPostsArray.length; i++) {
+            if ((photoPostsArray[i].id === photoPost.id) && (photoPost !== photoPostsArray[i])) {
                 return false;
             }
         }
         return true;
     }
+
     function addPhotoPost(photoPost) {
-        if(validatePhotoPost(photoPost)){
+        if (validatePhotoPost(photoPost)) {
             photoPostsArray.push(photoPost);
             return true;
         }
@@ -194,20 +196,22 @@ var photoPostsArray = [
             return false;
         }
     }
+
     function removePhotoPost(id) {
         if (typeof id !== "string" ||
             Number(id) < 1 ||
             Number(id) > photoPostsArray.length) {
             return NaN;
         }
-        for(var i = 0; i < photoPostsArray.length; i++){
-            if(photoPostsArray[i].id === id){
+        for (var i = 0; i < photoPostsArray.length; i++) {
+            if (photoPostsArray[i].id === id) {
                 delete photoPostsArray[i];
                 return true;
             }
         }
         return NaN;
     }
+
     function editPhotoPost(id, photoPost) {
         var sourcePhotoPost = getPhotoPost(id);
         if (sourcePhotoPost) {
@@ -225,5 +229,46 @@ var photoPostsArray = [
         else {
             return false;
         }
+    }
+
+    function getPhotoPosts(skip, top, filterConfig) {
+        if (typeof skip !== "number" ||
+            skip < 0) {
+            skip = 0;
+        }
+
+
+        if (typeof top !== "number" ||
+            top < 0) {
+            top = 10;
+        }
+
+
+        var filtPhotoPosts = photoPostsArray.sort(function (a, b) {
+            return b.createdAt - a.createdAt;
+        });
+
+
+        if ("createdAt" in filterConfig &&
+            typeof filterConfig.createdAt === "object" &&
+            Object.prototype.toString.call(filterConfig.createdAt) === "[object Date]") {
+            filtPhotoPosts = filtPhotoPosts.filter(function (item) {
+                return item.createdAt.getFullYear() === filterConfig.createdAt.getFullYear() &&
+                    item.createdAt.getMonth() === filterConfig.createdAt.getMonth() &&
+                    item.createdAt.getDay() === filterConfig.createdAt.getDay();
+            });
+        }
+
+
+        if ("author" in filterConfig &&
+            typeof filterConfig.author === "string" &&
+            filterConfig.author !== "") {
+            filtPhotoPosts = filtPhotoPosts.filter(function (item) {
+                return item.author === filterConfig.author;
+            });
+        }
+
+
+        return filtPhotoPosts.slice(skip, skip + top);
     }
 })();
